f19e1dd25443f5feb3edb75e36fcb7ff5c6e2172
Brandon Chang <brandon@brandonchang.me>
2022-07-28 15:28:31 -0400

    lambda runtime

--- src/routes/image.js
+++ src/routes/image.js
@@ -1,6 +1,15 @@
-import puppeteer from 'puppeteer';
-
 export async function GET(event) {
+	let chrome = {};
+	let puppeteer;
+	if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+		// running on the Vercel platform.
+		chrome = await import('chrome-aws-lambda');
+		puppeteer = await import('puppeteer-core');
+	} else {
+		// running locally.
+		puppeteer = await import('puppeteer');
+	}
+
 	const width = event.url.searchParams.get('width') || 400;
 	const height = event.url.searchParams.get('height') || 300;
 

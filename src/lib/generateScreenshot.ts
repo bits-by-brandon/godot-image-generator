1b097b7b7b78b5adc41c9d66da9ddb3d3234fbf2
Brandon Chang <brandon@brandonchang.me>
2022-07-29 00:25:49 -0400

    token authorization and upload

---
+++ src/lib/generateScreenshot.ts
@@ -0,0 +1,34 @@
+import playwright from 'playwright-aws-lambda';
+
+export default async function generateScreenshot(url: URL, width: number, height: number) {
+	url.pathname = 'og/preview';
+
+	const browser = await playwright.launchChromium({ headless: true }); // Or 'firefox' or 'webkit'.
+	const page = await browser.newPage();
+	await page.setViewportSize({ height, width });
+	await page.goto(url.toString(), { waitUntil: 'domcontentloaded' });
+	await page.evaluate(async () => {
+		const selectors = Array.from(document.querySelectorAll('img'));
+		await Promise.all([
+			document.fonts.ready,
+			...selectors.map((img) => {
+				// Image has already finished loading, let’s see if it worked
+				if (img.complete) {
+					// Image loaded and has presence
+					if (img.naturalHeight !== 0) return;
+					// Image failed, so it has no height
+					throw new Error('Image failed to load');
+				}
+				// Image has not loaded yet, added an event listener to know when it does
+				return new Promise((resolve, reject) => {
+					img.addEventListener('load', resolve);
+					img.addEventListener('error', reject);
+				});
+			})
+		]);
+	});
+
+	const screenshot = await page.screenshot();
+	await browser.close();
+	return screenshot;
+}
--- src/routes/og/image.ts
+++ src/routes/og/image.ts
@@ -1,40 +1,10 @@
 import type { RequestEvent } from '@sveltejs/kit';
-import playwright from 'playwright-aws-lambda';
+import generateScreenshot from '$lib/generateScreenshot';
 
 export async function GET(event: RequestEvent) {
 	const width = parseInt(event.url.searchParams.get('width') || '1200');
 	const height = parseInt(event.url.searchParams.get('height') || '630');
-
-	const domUrl = event.url;
-	domUrl.pathname = 'og/preview';
-
-	const browser = await playwright.launchChromium({ headless: true }); // Or 'firefox' or 'webkit'.
-	const page = await browser.newPage();
-	await page.setViewportSize({ height, width });
-	await page.goto(domUrl.toString(), { waitUntil: 'domcontentloaded' });
-	await page.evaluate(async () => {
-		const selectors = Array.from(document.querySelectorAll('img'));
-		await Promise.all([
-			document.fonts.ready,
-			...selectors.map((img) => {
-				// Image has already finished loading, let’s see if it worked
-				if (img.complete) {
-					// Image loaded and has presence
-					if (img.naturalHeight !== 0) return;
-					// Image failed, so it has no height
-					throw new Error('Image failed to load');
-				}
-				// Image has not loaded yet, added an event listener to know when it does
-				return new Promise((resolve, reject) => {
-					img.addEventListener('load', resolve);
-					img.addEventListener('error', reject);
-				});
-			})
-		]);
-	});
-
-	const screenshot = await page.screenshot();
-	await browser.close();
+	const screenshot = await generateScreenshot(event.url, width, height);
 
 	return {
 		status: 200,
